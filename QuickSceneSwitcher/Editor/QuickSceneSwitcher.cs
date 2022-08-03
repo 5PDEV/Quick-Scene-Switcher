47b621f54699d461ebce59fd4615689fab507a85
AlexejheroYTB <alexejhero@gmail.com>
2022-08-03 17:24:31 +0300

    Improve user experience
    - Remove colors
    - Disable editing in play mode
    - Remove saving of edit mode state

--- QuickSceneSwitcher/Editor/QuickSceneSwitcher.cs
+++ QuickSceneSwitcher/Editor/QuickSceneSwitcher.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class QuickSceneSwitcher : EditorWindow
 {
     private List<SceneAsset> scenes = null;
+    [Obsolete("", true)]
     private List<Color> buttonColors;
     private bool editMode = false;
 
@@ -24,7 +27,6 @@ public class QuickSceneSwitcher : EditorWindow
         PREFS_PREFIX = $"QuickSceneSwitcher/{Application.companyName}/{Application.productName}/";
 
         scenes = new List<SceneAsset>();
-        buttonColors = new List<Color>();
         AddScene();
 
         // Style vars
@@ -65,109 +67,121 @@ public class QuickSceneSwitcher : EditorWindow
         GUILayout.Label(" Quick Scenes", labelStyle);
         GUILayout.FlexibleSpace();
 
+        if (Application.isPlaying)
+        {
+            editMode = false;
+            GUI.enabled = false;
+        }
+        
         // Toggle 'EditMode' Button
-        GUI.backgroundColor = editMode ? GetButtonColorBasedOnEditorTheme(Color.white) : darkGray;
-        UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
+        // GUI.backgroundColor = editMode ? GetButtonColorBasedOnEditorTheme(Color.white) : darkGray;
+        // UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
         editMode = GUILayout.Toggle(editMode, editMode ? " Exit Edit Mode " : " Edit Mode ", buttonStyleBold);
+        
         GUI.backgroundColor = originalContentColor;
-
+        buttonStyleBold.normal.textColor = GUI.skin.button.normal.textColor;
+        buttonStyleBold.hover.textColor = GUI.skin.button.hover.textColor;
+        buttonStyleBold.focused.textColor = GUI.skin.button.focused.textColor;
+        buttonStyleBold.active.textColor = GUI.skin.button.active.textColor;
+        
         GUILayout.EndHorizontal();
         GUILayout.Space(3);
-
-        // Draw each scene button (or each scene options if in 'EditMode')
-        for (int i = 0; i < scenes.Count; i++)
+        
+        if (Application.isPlaying)
         {
-            if (editMode)
-            {
-                GUILayout.BeginHorizontal();
-
-                // Remove scene button
-                bool sceneRemoved = false;
-                if (GUILayout.Button("-", heightLayout, GUILayout.MaxWidth(25)))
-                {
-                    RemoveScene(i);
-                    sceneRemoved = true;
-                }
-
-                if (!sceneRemoved)
-                {
-                    // Move scene up and down buttons
-                    if (GUILayout.Button("↑", heightLayout, GUILayout.MaxWidth(25))) MoveScene(i, -1);
-                    if (GUILayout.Button("↓", heightLayout, GUILayout.MaxWidth(25))) MoveScene(i, 1);
-
-                    // Scene asset field
-                    scenes[i] = EditorGUILayout.ObjectField(scenes[i], typeof(SceneAsset), false, heightLayout) as SceneAsset;
-                    // Scene button color field
-                    buttonColors[i] = EditorGUILayout.ColorField(buttonColors[i], heightLayout, GUILayout.MaxWidth(80));
-                }
-
-                GUILayout.EndHorizontal();
-            }
-            else // if NOT in 'EditMode'
-            {
-                // Disable button if it corresponds to the currently open scene
-                bool isCurrentScene = IsCurrentScene(scenes[i]);
-                if (isCurrentScene) EditorGUI.BeginDisabledGroup(true);
-
-                // Setup button background and text colors
-                GUI.backgroundColor = GetButtonColorBasedOnEditorTheme(buttonColors[i]);
-                UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
-
-                // Draw scene button
-                if (scenes[i] != null && GUILayout.Button($"{scenes[i].name}{(isCurrentScene ? " (current)" : "")}", buttonStyleBold))
-                    OpenScene(scenes[i]);
-
-                GUI.backgroundColor = originalContentColor;
-                EditorGUI.EndDisabledGroup();
-            }
-        }
-
-        if (editMode) // 'EditMode' bottom options
-        {
-            GUILayout.Space(5);
-
-            // 'Add Scene' Button style
-            GUI.backgroundColor = darkGray;
-            UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
-            float previousHeight = buttonStyleBold.fixedHeight;
-            buttonStyleBold.fixedHeight = 25;
-
-            if (GUILayout.Button("Add Scene", buttonStyleBold))
-                AddScene();
-
-            GUILayout.Space(1);
-
-            // 'Load From BuildSettings' Button style
-            buttonStyleBold.fixedHeight = previousHeight;
-            GUI.backgroundColor = GetButtonColorBasedOnEditorTheme(originalContentColor);
-            UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
-
-            if (GUILayout.Button("Load From BuildSettings", buttonStyleBold))
-                LoadScenesFromBuildSettings();
+            GUILayout.Space(2);
+            GUILayout.Button("This tool is not usable in play mode.", GUILayout.Height(40), GUILayout.MinWidth(10));
+            GUI.enabled = true;
         }
         else
         {
-            // If NOT in 'EditMode' and there are no scenes buttons to show,
-            // display an info message about how to add them
-            if (scenes.Count == 0 || !(scenes.Any(i => i != null)))
+            // Draw each scene button (or each scene options if in 'EditMode')
+            for (int i = 0; i < scenes.Count; i++)
             {
-                GUILayout.Space(2);
-                EditorGUI.BeginDisabledGroup(true);
-                GUILayout.Button($"Enter 'EditMode' and add scenes\nfor them to appear here", GUILayout.Height(50), GUILayout.MinWidth(10));
-                EditorGUI.EndDisabledGroup();
-            }
-        }
+                if (editMode)
+                {
+                    GUILayout.BeginHorizontal();
 
-        // Draw Footer
-        if (editMode)
-        {
-            GUILayout.FlexibleSpace();
-            GUIStyle footerStyle = EditorStyles.centeredGreyMiniLabel;
-            Color footerColor = (EditorGUIUtility.isProSkin ? Color.white : Color.black);
-            footerColor.a = 0.25f;
-            footerStyle.normal.textColor = footerColor;
-            GUILayout.Label("Made by Kelvip", footerStyle);
-            GUILayout.Space(5);
+                    // Remove scene button
+                    bool sceneRemoved = false;
+
+                    if (GUILayout.Button("-", heightLayout, GUILayout.MaxWidth(25)))
+                    {
+                        RemoveScene(i);
+                        sceneRemoved = true;
+                    }
+
+                    if (!sceneRemoved)
+                    {
+                        // Move scene up and down buttons
+                        if (GUILayout.Button("↑", heightLayout, GUILayout.MaxWidth(25))) MoveScene(i, -1);
+                        if (GUILayout.Button("↓", heightLayout, GUILayout.MaxWidth(25))) MoveScene(i, 1);
+
+                        // Scene asset field
+                        scenes[i] = EditorGUILayout.ObjectField(scenes[i], typeof(SceneAsset), false, heightLayout) as SceneAsset;
+                        // Scene button color field
+                        // buttonColors[i] = EditorGUILayout.ColorField(buttonColors[i], heightLayout, GUILayout.MaxWidth(80));
+                    }
+
+                    GUILayout.EndHorizontal();
+                }
+                else // if NOT in 'EditMode'
+                {
+                    // Disable button if it corresponds to the currently open scene
+                    bool isCurrentScene = IsCurrentScene(scenes[i]);
+                    if (isCurrentScene) EditorGUI.BeginDisabledGroup(true);
+
+                    // Setup button background and text colors
+                    // GUI.backgroundColor = GetButtonColorBasedOnEditorTheme(buttonColors[i]);
+                    // UpdateButtonStyleTextColor(GUI.backgroundColor, buttonStyleBold);
+
+                    // Draw scene button
+                    if (scenes[i] != null && GUILayout.Button($"{scenes[i].name}{(isCurrentScene ? " (current)" : "")}", buttonStyleBold)) OpenScene(scenes[i]);
+
+                    GUI.backgroundColor = originalContentColor;
+                    EditorGUI.EndDisabledGroup();
+                }
+            }
+
+            if (editMode) // 'EditMode' bottom options
+            {
+                GUILayout.Space(5);
+
+                // 'Add Scene' Button style
+                float previousHeight = buttonStyleBold.fixedHeight;
+                buttonStyleBold.fixedHeight = 25;
+                if (GUILayout.Button("Add Scene", buttonStyleBold)) AddScene();
+
+                GUILayout.Space(1);
+
+                // 'Load From BuildSettings' Button style
+                buttonStyleBold.fixedHeight = previousHeight;
+                if (GUILayout.Button("Load From BuildSettings", buttonStyleBold)) LoadScenesFromBuildSettings();
+            }
+            else
+            {
+                // If NOT in 'EditMode' and there are no scenes buttons to show,
+                // display an info message about how to add them
+                if (scenes.Count == 0 || !(scenes.Any(i => i != null)))
+                {
+                    GUILayout.Space(2);
+                    EditorGUI.BeginDisabledGroup(true);
+                    GUILayout.Button($"Enter 'EditMode' and add scenes\nfor them to appear here", GUILayout.Height(50), GUILayout.MinWidth(10));
+                    EditorGUI.EndDisabledGroup();
+                }
+            }
+
+            // Draw Footer
+            if (editMode)
+            {
+                GUILayout.FlexibleSpace();
+                GUIStyle footerStyle = EditorStyles.centeredGreyMiniLabel;
+                Color footerColor = (EditorGUIUtility.isProSkin ? Color.white : Color.black);
+                footerColor.a = 0.25f;
+                footerStyle.normal.textColor = footerColor;
+                GUILayout.Label("", footerStyle);
+                GUILayout.Space(5);
+            }
         }
 
         GUILayout.EndScrollView();
@@ -179,6 +193,7 @@ public class QuickSceneSwitcher : EditorWindow
     #region AuxiliarFunctions
 
     // This function chooses black or white for the scene button texts based on the color of the button
+    [Obsolete("", true)]
     private void UpdateButtonStyleTextColor(Color color, GUIStyle style)
     {
         Color.RGBToHSV(color, out float h, out float s, out float v);
@@ -198,6 +213,7 @@ public class QuickSceneSwitcher : EditorWindow
     }
 
     // Returns the buttonColor compensating for the editor theme skin
+    [Obsolete("", true)]
     private Color GetButtonColorBasedOnEditorTheme(Color buttonColor)
     {
         // If isProSkin is true, the editor is in dark mode
@@ -228,14 +244,14 @@ public class QuickSceneSwitcher : EditorWindow
     private void AddScene()
     {
         scenes.Add(null);
-        buttonColors.Add(Color.white);
+        // buttonColors.Add(Color.white);
     }
 
     // Remove the specified scene slot
     private void RemoveScene(int index)
     {
         scenes.RemoveAt(index);
-        buttonColors.RemoveAt(index);
+        // buttonColors.RemoveAt(index);
         SavePrefs();
     }
 
@@ -247,11 +263,11 @@ public class QuickSceneSwitcher : EditorWindow
         if (newIndex != index)
         {
             SceneAsset scene = scenes[index];
-            Color buttonColor = buttonColors[index];
+            // Color buttonColor = buttonColors[index];
             RemoveScene(index);
 
             scenes.Insert(newIndex, scene);
-            buttonColors.Insert(newIndex, buttonColor);
+            // buttonColors.Insert(newIndex, buttonColor);
         }
         SavePrefs();
     }
@@ -292,6 +308,8 @@ public class QuickSceneSwitcher : EditorWindow
     // Save current preferences to persistent data
     private void SavePrefs()
     {
+        
+        
         // Scenes Count
         EditorPrefs.SetInt(PREFS_PREFIX + "ScenesCount", scenes.Count);
 
@@ -301,12 +319,12 @@ public class QuickSceneSwitcher : EditorWindow
             EditorPrefs.SetString(PREFS_PREFIX + $"Scene_{i}", AssetDatabase.GetAssetPath(scenes[i]));
 
             // Button Colors
-            string sceneColor = ColorUtility.ToHtmlStringRGBA(buttonColors[i]);
-            EditorPrefs.SetString(PREFS_PREFIX + $"SceneColor_{i}", $"#{sceneColor}");
+            // string sceneColor = ColorUtility.ToHtmlStringRGBA(buttonColors[i]);
+            // EditorPrefs.SetString(PREFS_PREFIX + $"SceneColor_{i}", $"#{sceneColor}");
         }
 
         // Edit Mode
-        EditorPrefs.SetBool(PREFS_PREFIX + $"EditMode", editMode);
+        // EditorPrefs.SetBool(PREFS_PREFIX + $"EditMode", editMode);
     }
 
     // Load and parse preferences from persistent saved data
@@ -316,7 +334,7 @@ public class QuickSceneSwitcher : EditorWindow
         int scenesCount = EditorPrefs.GetInt(PREFS_PREFIX + "ScenesCount");
 
         scenes.Clear();
-        buttonColors.Clear();
+        // buttonColors.Clear();
 
         for (int i = 0; i < scenesCount; i++)
         {
@@ -326,13 +344,13 @@ public class QuickSceneSwitcher : EditorWindow
             scenes[i] = AssetDatabase.LoadAssetAtPath<SceneAsset>(scenePath);
 
             // Button Colors
-            string sceneColorString = EditorPrefs.GetString(PREFS_PREFIX + $"SceneColor_{i}");
-            ColorUtility.TryParseHtmlString(sceneColorString, out Color sceneColor);
-            buttonColors[i] = sceneColor;
+            // string sceneColorString = EditorPrefs.GetString(PREFS_PREFIX + $"SceneColor_{i}");
+            // ColorUtility.TryParseHtmlString(sceneColorString, out Color sceneColor);
+            // buttonColors[i] = sceneColor;
         }
 
         // Edit Mode
-        editMode = EditorPrefs.GetBool(PREFS_PREFIX + $"EditMode");
+        // editMode = EditorPrefs.GetBool(PREFS_PREFIX + $"EditMode");
     }
 
     #endregion
